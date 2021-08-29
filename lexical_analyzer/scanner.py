5ecaef1985b102edc9232bc7ec69c5f3096bafb3
Juliana Resplande <julianarsg13@gmail.com>
2021-08-29 17:48:29 -0300

    fix scanner

--- lexical_analyzer/scanner.py
+++ lexical_analyzer/scanner.py
@@ -4,6 +4,7 @@ from afd import AFD
 from token_ import (
     RESERVED_WORDS,
     CLASS_MAPPING,
+    ERROR_STATE,
     Token,
 )
 
@@ -17,27 +18,43 @@ class Scanner:
     def get_positions(self) -> tuple:
         return self._pos.get_values()
 
-    def scanner(self, file) -> Token:
-        state = 0
-        lexeme = ""
-        char = "start"
+    def _is_reserved_word(self, lexeme):
+        return lexeme in RESERVED_WORDS
 
-        while self._pos.update():
+    def _found_token(self, prev_token_class, lexeme, char, state) -> bool:
+        is_normal_token = prev_token_class != None and state == ERROR_STATE
+        is_space = char in [" ", "\n", "\t"]
+
+        return self._is_reserved_word(lexeme) or is_normal_token or is_space
+
+    def scanner(self, file) -> Token:
+        char = "start"
+        lexeme = ""
+        state = 0
+        token_class = None
+
+        while self._pos.update(char):
             char = file.read(1) if char != "" else "$"
+
+            prev_state = state
+            prev_token_class = token_class
 
             state = self._afd.run(char, state)
             token_class = CLASS_MAPPING.get(state)
 
-            if token_class == None and not lexeme in RESERVED_WORDS:
-                lexeme = lexeme + char
-            else:
+            if self._found_token(self, prev_token_class, state, lexeme):
+                if self._is_reserved_word(lexeme):  # not found in current cursor
+                    file.seek(-1, 1)  # goes back one word
+                    state = prev_state
+                    token_class = prev_token_class
+
                 if token_class == ["id"]:
-                    token = self.symb_table.find(lexeme)
+                    token = self._symb_table.find(lexeme)
 
                 if not (token_class == ["id"] and token != None):
                     token = Token(lexema=lexeme, classe=token_class)
 
                     if token_class == ["id"]:
-                        self.symb_table.insert(token)
-
-                return token
+                        self._symb_table.insert(token)
+            else:
+                lexeme = lexeme + char
