061fc5c2d9224061be7020a73d96befd8f81ba28
Juliana Resplande <julianarsg13@gmail.com>
2021-09-26 22:59:35 -0300

    panic recovery

--- src/mgol/syntactic/parser.py
+++ src/mgol/syntactic/parser.py
@@ -1,4 +1,6 @@
+import json
 from typing import TextIO
+
 
 import pandas as pd
 
@@ -10,8 +12,12 @@ class Parser:
     def __init__(self):
         self.stack = [0]
         self.scanner = Scanner()
+
         self.action = pd.read_table("srl_table/action.tsv")
         self.goto = pd.read_table("srl_table/goto.tsv")
+        with open("srl_table/grammar.json") as f:
+            self.grammar = json.load(f)
+
         self.recovery = PanicRecovery()
 
     def parse(self, file: TextIO):
@@ -19,19 +25,24 @@ class Parser:
 
         while True:
             state = self.stack.pop()
-            action, state = self.action[state][token.lemma]
+            action, number = self.action[state][token.lemma]
 
-            if action == "shift":
-                self.stack.append(state)
+            if action == "s":
+                self.stack.append(number)
                 token = self.scanner.scan(file)
-            elif action == "reduce":
-                # desempilha os simbolos
-                self.stack.append(t)  # quem é o t??
-                state = self.goto[t][A]  # quem é a??
+            elif action == "r":
+                grammar_rule = self.grammar[number]  # enumerar regras a partir do 0
+
+                for _ in grammar_rule[1:]:
+                    state = self.stack.pop()
+
+                state = self.goto[state][self.grammar_rule[0]]
                 self.stack.append(state)
-                # imprimir regra que foi reduzida
-            elif action == "accept":
+
+                grammar_rule = grammar_rule[0] + " -> " + " ".join(grammar_rule[1:])
+                print(grammar_rule)
+            elif action == "a":
                 break
             else:
-                self.recovery.recover()
+                self.stack, self.scanner = self.recovery.recover(file, self.scanner)
                 # imprimir onde ocorreu o erro e o tipo
--- src/mgol/syntactic/recovery.py
+++ src/mgol/syntactic/recovery.py
@@ -1,3 +1,18 @@
+from typing import TextIO
+
+from mgol.lexical.scanner import Scanner
+
+
 class PanicRecovery:
-    def recover():
-        pass
+    sync_words = {")", "}", ";"}  # confirmar
+
+    def recover(self, file: TextIO, scanner: Scanner):
+        stack = [0]
+
+        while scanner._pos.update("start", ""):
+            char = file.read(1)
+
+            if char in self.sync_words:
+                break
+
+        return stack, scanner
