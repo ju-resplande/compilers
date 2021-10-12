bff8b7c27e96f87f551825435d5ca8a40827a15a
Juliana Resplande <julianarsg13@gmail.com>
2021-10-12 11:03:37 -0300

    error msg

--- src/mgol/syntactic/parser.py
+++ src/mgol/syntactic/parser.py
@@ -5,7 +5,6 @@ from typing import TextIO, List
 
 import pandas as pd
 
-from mgol.lexical.token_ import Token
 from mgol.utils import print_error_msg
 from mgol.lexical.scanner import Scanner
 from mgol.syntactic.recovery import PanicRecovery
@@ -39,33 +38,34 @@ class Parser:
         token_class = token.classe.lower()  # srl table uses only lowercase
         token_class = "$" if token_class == "eof" else token_class  # srl table uses $
 
-        return token_class, token  # for debug and error_msg reasons
+        return token_class, token  # returns token for debugging
 
     def print_grammar_rule(self, grammar_rule: List[str]):
         grammar_rule = grammar_rule[0] + " -> " + " ".join(grammar_rule[1:])
 
         print(grammar_rule)
 
-    def print_error_msg(self, state: str, number: int, prev_token: Token):
+    def print_error_msg(
+        self, state: str, number: int, prev_token_class: str, token_class: str
+    ):
         state_actions = self.action.iloc[state]
 
         expected = state_actions[
             state_actions.str.contains(r"^r|s", regex=True, na=False)
         ].index.tolist()
 
-        print(expected)
-
-        # map symbol names?
-
         cur_pos = self.scanner.get_positions()
-        err_desc = f"Esperados as palavras {expected} após {prev_token}"
+        err_desc = (
+            f'Esperava-se um token entre "{expected}" após {prev_token_class} \n'
+            f'Mas recebeu "{token_class}" no lugar'
+        )
         print_error_msg(
             "Erro sintático", f"ERRO{number}", err_desc, cur_pos,
         )
 
     def parse(self, file: TextIO):
         token_class, token = self.get_next_symbol(file)
-        prev_token = "o começo do programa"
+        prev_token_class = "o começo do programa"
 
         while True:
             state = self.stack[-1]
@@ -79,19 +79,19 @@ class Parser:
             action, number = action_number[0], int(action_number[1:])
 
             if self.debug:
-                print(f'action: {"reduce" if action == "r" else "shift"}')
-                print(f"number: {number}")
                 print(
-                    f"token class: -{token.classe}- token lexema: -{token.lexema}- token tipo: -{token.tipo}-"
+                    f'action: {"reduce" if action == "r" else "shift"} \n'
+                    f"number: {number} \n"
                 )
+                print(token)
 
             if action == "e":
-                self.print_error_msg(state, number, prev_token)
+                self.print_error_msg(state, number, prev_token_class, token_class)
                 self.stack, self.scanner = self.recovery.recover(file, self.scanner)
             elif action == "s":
                 self.stack.append(number)
 
-                prev_token = token
+                prev_token_class = token_class
                 token_class, token = self.get_next_symbol(file)
             elif action == "r":
                 grammar_rule = self.grammar[number - 1]
--- src/mgol/utils.py
+++ src/mgol/utils.py
@@ -3,5 +3,5 @@ from typing import List
 
 def print_error_msg(err_type: str, err_name: str, err_desc: str, cur_pos: List[int]):
     print(
-        f"{err_type}: {err_name} - {err_desc}, linha {cur_pos[0]}, coluna {cur_pos[1]}",
+        f"{err_type}: {err_name} - {err_desc}: linha {cur_pos[0]}, coluna {cur_pos[1]}",
     )
