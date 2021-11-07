837f86c759f55f076a51ed6033152bc0309ab891
Juliana Resplande <julianarsg13@gmail.com>
2021-11-07 14:56:06 -0300

    printf \n at end

--- src/mgol/semantic/semantics.py
+++ src/mgol/semantic/semantics.py
@@ -137,11 +137,11 @@ class Semantics:
             arg = grammar_tokens["ARG"][0]
 
             if arg.tipo == "literal":
-                self._imprimir(self._make_ident() + f'printf("%s", {arg.lexema});\n')
+                self._imprimir(self._make_ident() + f'printf("%s", {arg.lexema}\n);\n')
             elif arg.tipo == "inteiro":
-                self._imprimir(self._make_ident() + f'printf("%d", {arg.lexema});\n')
+                self._imprimir(self._make_ident() + f'printf("%d", {arg.lexema}\n);\n')
             elif arg.tipo == "real":
-                self._imprimir(self._make_ident() + f'printf("%lf", {arg.lexema});\n')
+                self._imprimir(self._make_ident() + f'printf("%lf", {arg.lexema}\n);\n')
         elif rule_number == 15:
             self._copy_attrs(grammar_tokens["lit"][0], grammar_tokens["ARG"][0])
         elif rule_number == 16:
